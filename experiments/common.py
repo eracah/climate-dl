23fd407c2082fd107c2877556302d6744745678d
eracah <ejracah@gmail.com>
2016-09-21 16:04:06 -0700

    added train function to common

--- experiments/common.py
+++ experiments/common.py
@@ -3,15 +3,223 @@ from os import listdir, system
 from os.path import isfile, join, isdir
 import re
 import numpy as np
-from matplotlib import pyplot as plt
 import os
 import sys
 from shutil import copyfile
 import imp
 from lasagne.layers import *
 from sklearn.manifold import TSNE
-
 import itertools
+import theano
+from theano import tensor as T
+import lasagne
+from lasagne.objectives import *
+from lasagne.nonlinearities import *
+from lasagne.updates import *
+from lasagne.utils import *
+import numpy as np
+import cPickle as pickle
+import gzip
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from matplotlib import pyplot
+import os
+import sys
+from time import time
+sys.path.append("..")
+#import common
+#sys.path.append("stl10/")
+# import stl10
+from psutil import virtual_memory
+from pylab import rcParams
+rcParams['figure.figsize'] = 15, 20
+import pdb
+import logging
+
+
+# -------------------------------------
+
+def get_net(net_cfg, args):
+    l_out, ladder = net_cfg(args)
+    if args["mode"] == "2d":
+        X = T.tensor4('X')
+    elif args["mode"] == "3d":
+        X = T.tensor5('X')
+    #net_out = get_output(l_out, X)
+    ladder_output = get_output([l_out] + ladder, X)
+    net_out = ladder_output[0]
+    # squared error loss is between the first two terms
+    loss = squared_error(net_out, X).mean()
+    sys.stderr.write("main loss between %s and %s\n" % (str(l_out.output_shape), "X") )
+    ladder_output = ladder_output[1::]
+    if "ladder" in args:
+        sys.stderr.write("using ladder connections for conv\n")        
+        for i in range(0, len(ladder_output), 2):
+            sys.stderr.write("ladder connection between %s and %s\n" % (str(ladder[i].output_shape), str(ladder[i+1].output_shape)) )
+            assert ladder[i].output_shape == ladder[i+1].output_shape
+            loss += args["ladder"]*squared_error(ladder_output[i], ladder_output[i+1]).mean()
+    
+    net_out_det = get_output(l_out, X, deterministic=True)
+    # this is deterministic + doesn't have the reg params added to the end
+    loss_det = squared_error(net_out_det, X).mean()
+    params = get_all_params(l_out, trainable=True)
+    lr = theano.shared(floatX(args["learning_rate"]))
+    if "optim" not in args:
+        updates = nesterov_momentum(loss, params, learning_rate=lr, momentum=0.9)
+    else:
+        if args["optim"] == "rmsprop":
+            updates = rmsprop(loss, params, learning_rate=lr)
+        elif args["optim"] == "adam":
+            updates = adam(loss, params, learning_rate=lr)
+    
+    #updates = adadelta(loss, params, learning_rate=lr)
+    #updates = rmsprop(loss, params, learning_rate=lr)
+    train_fn = theano.function([X], [loss, loss_det], updates=updates)
+    loss_fn = theano.function([X], loss)
+    out_fn = theano.function([X], net_out_det)
+    return {
+        "train_fn": train_fn,
+        "loss_fn": loss_fn,
+        "out_fn": out_fn,
+        "lr": lr,
+        "l_out": l_out
+    }
+
+
+def iterate(X_train, bs=32):
+    b = 0
+    while True:
+        if b*bs >= X_train.shape[0]:
+            break
+        yield X_train[b*bs:(b+1)*bs]
+        b += 1
+    
+        
+def get_iterator(name, batch_size, data_dir, start_day, end_day, img_size, time_chunks_per_example, step_size):
+    # for stl10, 'days' and 'data_dir' does not make
+    # any sense
+    assert name in ["climate", "stl10"]
+    if name == "climate":
+        return data_iterator(batch_size, data_dir, start_day=start_day, end_day=end_day, img_size=img_size, time_chunks_per_example=time_chunks_per_example, step_size=step_size)
+    elif name == "stl10":
+        return stl10.data_iterator(batch_size)
+        
+def train(cfg,
+        num_epochs,
+        out_folder,
+        sched={},
+        batch_size=128,
+        model_folder="/storeSSD/cbeckham/nersc/models/",
+        tmp_folder="tmp",
+        training_days=[1,20],
+        validation_days=[345,365],
+        time_chunks_per_example=1,
+        step_size=20,
+        data_dir="/storeSSD/cbeckham/nersc/big_images/",
+        dataset="climate",
+        img_size=128,
+        resume=None,
+        debug=True):
+    
+    def prep_batch(X_batch):
+        if dataset == "climate":
+            if time_chunks_per_example == 1:
+                # shape is (32, 1, 16, 128, 128), so collapse to a 4d tensor
+                X_batch = X_batch.reshape(X_batch.shape[0], X_batch.shape[2], X_batch.shape[3], X_batch.shape[4])
+            else:
+                # right now it is: (bs, time, nchannels, height, width)
+                # needs to be: (bs, nchannels, time, height, width)
+                X_batch = X_batch.reshape(X_batch.shape[0], X_batch.shape[2], X_batch.shape[1], X_batch.shape[3], X_batch.shape[4])
+        else:
+            pass # nothing needs to be done for stl-10
+        return X_batch
+
+    def plot_image(img_composite):
+        if dataset == "climate":
+            for j in range(0,32):
+                plt.subplot(8,4,j+1)
+                if time_chunks_per_example > 1:
+                    plt.imshow(img_composite[j][0])
+                else:
+                    plt.imshow(img_composite[j])
+                plt.axis('off')
+        elif dataset == "stl10":
+            for j in range(0,6):
+                plt.subplot(3,2,j+1)
+                plt.imshow(img_composite[j])
+                plt.axis('off')
+        plt.savefig('%s/%i.png' % (out_folder, epoch))
+        pyplot.clf()
+        
+    # extract methods
+    train_fn, loss_fn, out_fn, l_out = cfg["train_fn"], cfg["loss_fn"], cfg["out_fn"], cfg["l_out"]
+    lr = cfg["lr"]
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+    #if resume != None:
+    #    
+    
+    def get_logger():
+        logger = logging.getLogger('log_train')
+        logger.setLevel(logging.INFO)
+        fh = logging.FileHandler('%s/results.txt'%(out_folder))
+        fh.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        logger.addHandler(ch)
+        logger.addHandler(fh)
+        return logger
+    
+    logger = get_logger()
+    for epoch in range(0, num_epochs):
+        t0 = time()
+        # learning rate schedule
+        if epoch+1 in sched:
+            lr.set_value( floatX(sched[epoch+1]) )
+            logger.info("changing learning rate to: %f\n" % sched[epoch+1])
+        train_losses = []
+        train_losses_det = []
+        valid_losses = []
+        first_minibatch = True
+        # TRAINING LOOP
+        for X_train, y_train in get_iterator(dataset, batch_size, data_dir, start_day=training_days[0], end_day=training_days[1],
+                                        img_size=img_size,step_size=step_size,time_chunks_per_example=time_chunks_per_example):
+            X_train = prep_batch(X_train)
+            if first_minibatch:
+                X_train_sample = X_train[0:1]
+                first_minibatch = False
+            this_loss, this_loss_det = train_fn(X_train)
+            if debug:
+                logger.info("iteration loss: %f" %this_loss)
+            train_losses.append(this_loss)
+            train_losses_det.append(this_loss_det)
+        if debug:
+            mem = virtual_memory()
+            print mem
+        # VALIDATION LOOP
+        for X_valid, y_valid in get_iterator(dataset, batch_size, data_dir, start_day=validation_days[0], end_day=validation_days[1],
+                                              img_size=img_size, time_chunks_per_example=time_chunks_per_example, step_size=step_size):
+            X_valid = prep_batch(X_valid)
+            val_loss = loss_fn(X_valid)
+            if debug:
+                logger.info("iteration val loss: %f" %val_loss)
+            valid_losses.append(val_loss)
+        # DEBUG: visualise the reconstructions
+        img_orig = X_train_sample
+        img_reconstruct = out_fn(img_orig)
+        img_composite = np.vstack((img_orig[0],img_reconstruct[0]))
+        plot_image(img_composite)
+        # STATISTICS
+        time_taken = time() - t0
+
+        logger.info("epoch %i of %i \n time: %f \n train loss: %6.3f \n val loss: %6.3f" % (epoch+1, num_epochs, time_taken, np.mean(train_losses), np.mean(valid_losses)))
+
+        # save model at each epoch
+        if not os.path.exists("%s/%s" % (model_folder, out_folder)):
+            os.makedirs("%s/%s" % (model_folder, out_folder))
+        with open("%s/%s/%i.model" % (model_folder, out_folder, epoch), "wb") as g:
+            pickle.dump( get_all_param_values(cfg["l_out"]), g, pickle.HIGHEST_PROTOCOL )
 
 def make_dense_conv_encoder(args):
     conv_kwargs = dict(filter_size=3, pad=1, nonlinearity=args['nonlinearity'], W=lasagne.init.HeNormal())
@@ -209,7 +417,7 @@ def get_chunk_class(boxes, chunk_coords):
         ioba = calc_int_over_box_area(chunk_coords, box_coords)
         # if the intersection of the chunk with the box 
         #normalized by box area is over 0.75, then we label the chunk as that class
-        if ioba > 0.75:
+        if ioba >= 0.75:
             cls = box_class
             break
 
