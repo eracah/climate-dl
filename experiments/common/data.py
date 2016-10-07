580d75aea47f688c582a69464dade601f93432d2
eracah <ejracah@gmail.com>
2016-10-06 17:02:08 -0700

    changed iterator

--- experiments/common/data.py
+++ experiments/common/data.py
@@ -11,9 +11,9 @@ import numpy as np
 import cPickle as pickle
 import gzip
 import matplotlib
-matplotlib.use('agg')
+#matplotlib.use('agg')
 import matplotlib.pyplot as plt
-from matplotlib import pyplot
+%matplotlib inline
 import os
 import sys
 from time import time
@@ -25,6 +25,9 @@ import itertools
 
 
 # -------------------------------------
+
+#tropical depression are 0
+# hurricanes are 1
 
 def make_time_slice(dataset, time, variables, x=768, y=1152):
     '''Takes in a dataset, a time and variables and gets one time slice for all the variables and all x and y'''
@@ -51,45 +54,46 @@ def make_labels_for_dataset(dataset, time_steps):
                                                                u'teca_ymin',
                                                                u'teca_ymax',
                                                                u'teca_category']] ) for time_step in range(time_steps) ]
+    labels_list = edit_classes(labels_list)
+    return labels_list
 
+def edit_classes(labels_list):
+    for i in range(len(labels_list)):
+        for j in range(len(labels_list[i])):
+            tup = list(labels_list[i][j])
+            if labels_list[i][j][4] == -1:
+                tup[4] = 0
+                labels_list[i][j] = tuple(tup)
+            else:
+                tup[4] = 1
+                labels_list[i][j] = tuple(tup)
     return labels_list
     
+def make_masks_for_dataset(dataset, time_steps, classes=2):
+    labels_list = make_labels_for_dataset(dataset, time_steps)
+    x, y = dataset['TMQ'][0].shape
+    masks = np.zeros((time_steps, classes, x ,y))
+    bg = np.ones((time_steps, 1, x ,y))
+    for time_step, labels in enumerate(labels_list):
+        
+        for label in labels:
+            cls = label[-1]
+            x_slice = slice(label[0],label[1])
+            y_slice = slice(label[2], label[3])
+            masks[time_step, cls, y_slice,  x_slice] = 1
+            bg[time_step, 0,y_slice, x_slice] = 0
+    masks = np.concatenate((masks, bg), axis=1).reshape(time_steps, classes+1, x ,y)
+    #masks is an 8,num_classes, 768, 1152 mask 0's everywhere except where class is
+    return masks
 
-def calc_int_over_box_area(coords_1, box_coords):
-    ''' calc intersection over box area'''
-    xmin1,xmax1, ymin1, ymax1 = coords_1
-    xmin2,xmax2, ymin2, ymax2 = box_coords
-    inters = max(0,(min(xmax1,xmax2) - max(xmin1,xmin2)))   * \
-                          max(0,(min(ymax1,ymax2) - max(ymin1,ymin2)) )
-    def get_area(box_mm):
-        xmin, xmax, ymin, ymax = box_mm
-        return (xmax - xmin) * (ymax - ymin)
-    box_area = get_area((xmin2, xmax2, ymin2, ymax2))                                                     
-    
-    return inters / float(box_area)
 
-
-def get_chunk_class(boxes, chunk_coords):
-    
-    # for some reason there are classes that are less than 0, so we set
-    # the none class to -3
-    cls=-3
-    for box in boxes:
-        box_coords = box[:4]
-        box_class = box[-1]
-        ioba = calc_int_over_box_area(chunk_coords, box_coords)
-        # if the intersection of the chunk with the box 
-        #normalized by box area is over 0.75, then we label the chunk as that class
-        if ioba >= 0.75:
-            cls = box_class
-            break
-
-    return cls
-    
+                
+                
 
 import pdb
 
-def _day_iterator(year=1979, data_dir="/project/projectdirs/dasrepo/gordon_bell/climate/data/big_images/", shuffle=False, start_day=1, end_day=365, month1='01', day1='01', time_steps=8):
+def _day_iterator(year=1979, data_dir="/project/projectdirs/dasrepo/gordon_bell/climate/data/big_images/", shuffle=False, start_day=1, end_day=365,
+                  month1='01', day1='01', time_steps=8, classes=2):
     """
     This iterator will return tensors of dimension (8, 16, 768, 1152) 
     each tensor corresponding to one of the 365 days of the year
@@ -119,6 +123,7 @@ def _day_iterator(year=1979, data_dir="/project/projectdirs/dasrepo/gordon_bell/
 
     prefix = 'cam5_1_amip_run2.cam2.h2.1979'
     suffix = '00000.nc'
+    #print camfiles
     ind= camfiles.index('-'.join([prefix, month1, day1, suffix]))
     camfiles = camfiles[ind+(start_day-1):ind+end_day] # [
     if shuffle:
@@ -129,38 +134,21 @@ def _day_iterator(year=1979, data_dir="/project/projectdirs/dasrepo/gordon_bell/
         y=1152
         day_slice = make_spatiotemporal_tensor(dataset,time_steps,variables) #one day slice per dataset
         tr_data = day_slice.reshape(time_steps,len(variables), x, y)
-        labels = make_labels_for_dataset(dataset, time_steps)
-        yield tr_data, labels
+        masks = make_masks_for_dataset(dataset, time_steps, classes=classes)
+        
+        yield tr_data, masks
 
 
-def get_slices(img, labels, img_size=128, step_size=20):
-    '''
-    input: time_chunk_size, 16, 768,1152 tensor
-    returns: iterator of overlapping time_chunk_size,16,128,128 tensors'''
-    slices = []
-    classes = []
-    height, width = img.shape[-2], img.shape[-1]
-    for y in range(0, height, step_size):
-        for x in range(0, width, step_size):
-            chunk = img[:,:, y:y+img_size, x:x+img_size]
-            cls = get_chunk_class(labels, (x, x+img_size, y, y+img_size))
-            #classes.append(cls)
-            if chunk.shape[2:] == (img_size,img_size):
-                #slices.append(chunk)
-                yield chunk, cls
-    #pdb.set_trace()
-    #return np.asarray(slices, dtype=img.dtype), np.asarray(classes)
     
 def data_iterator(batch_size,
-                  data_dir="/project/projectdirs/dasrepo/gordon_bell/climate/data/big_images/",
-                  time_chunks_per_example=1,
+                  data_dir="/project/projectdirs/dasrepo/gordon_bell/deep_learning/data/climate/big_images/",
+                  time_chunks_per_example=8,
                   shuffle=False,
-                  step_size=20,
                   start_day=1,
                   end_day=365,
                   month1='01',
                   day1='01',
-                  time_steps=8):
+                  time_steps=8, classes=2):
     '''
     Args:
        batch_size: number of examples in a batch
@@ -170,7 +158,7 @@ def data_iterator(batch_size,
     '''
     # for each day (out of 365 days)
     day=0
-    for tensor, labels in _day_iterator(data_dir=data_dir, shuffle=shuffle, start_day=start_day, end_day=end_day, month1=month1, day1=day1, time_steps=time_steps):  #tensor is 8,16,768,1152
+    for tensor, masks in _day_iterator(data_dir=data_dir, shuffle=shuffle, start_day=start_day, end_day=end_day, month1=month1, day1=day1, time_steps=time_steps):  #tensor is 8,16,768,1152
         # preprocess for day
         tensor, min_, max_ = normalize(tensor)
         #TODO: preprocess over everything
@@ -182,6 +170,8 @@ def data_iterator(batch_size,
         #reshapes the tensor into multiple spatiotemporal chunks of (chunk_size, 16, 768,1152)
         spatiotemporal_tensor = tensor.reshape(time_chunks_per_day / time_chunks_per_example, 
                                                time_chunks_per_example, variables, h ,w)
+        sp_mask = masks.reshape(time_chunks_per_day / time_chunks_per_example, 
+                                               time_chunks_per_example,classes+1, h ,w)
         
         if shuffle:
             np.random.shuffle(spatiotemporal_tensor)
@@ -191,10 +181,11 @@ def data_iterator(batch_size,
             if b*batch_size >= spatiotemporal_tensor.shape[0]:
                 break
             # todo: add labels
-            yield spatiotemporal_tensor[b*batch_size:(b+1)*batch_size], None
+            yield spatiotemporal_tensor[b*batch_size:(b+1)*batch_size], sp_mask[b*batch_size:(b+1)*batch_size]
             b += 1
 
-def consecutive_day_iterator_3d(start_day, end_day, data_dir="/project/projectdirs/dasrepo/gordon_bell/climate/data/big_images/", shuffle=False):
+def consecutive_day_iterator_3d(start_day, end_day, data_dir="/project/projectdirs/dasrepo/gordon_bell/deep_learning/data/climate/big_images/", 
+                                shuffle=False, classes=2, labels_only=True):
     def imerge(a,b):
         for i,j in itertools.izip(a,b):
             yield i,j
@@ -205,10 +196,26 @@ def consecutive_day_iterator_3d(start_day, end_day, data_dir="/project/projectdi
     # e.g. combined iterator will return: (day1, day2), (day2, day3), ..., (day 364, day 365)
     for tp1, tp2 in imerge(iter1, iter2):
         x1 = tp1[0]
-        #y1 = tp1[1]
+        y1 = tp1[1]    
         x2 = tp2[0]
-        #y2 = tp2[1]
-        yield x1, x2
+        y2 = tp2[1]
+        if labels_only:
+            x = np.concatenate((x1[:,[0,2,4,6]],x2[:, [0,2,4,6]]),axis=1)
+            y = np.concatenate((y1[:, [0,2,4,6]],y2[:, [0,2,4,6]]), axis=1)
+            x = np.swapaxes(x,1,2 )
+            yield x,y
+        else:
+            yield (x1,y1), (x2,y2)
+def segmentation_iterator(start_day, end_day, data_dir="/project/projectdirs/dasrepo/gordon_bell/deep_learning/data/climate/big_images/", 
+                                shuffle=False, classes=2, labels_only=True ):
+    for x,y in data_iterator(batch_size=1, data_dir=data_dir, time_chunks_per_example=8, 
+                  shuffle=shuffle, start_day=start_day+1, end_day=end_day, classes=classes):
+        if labels_only:
+            yield x[:,[0,2,4,6]], y[:,[0,2,4,6]]
+        else:
+            yield x,y
+        
+            
 
                 
 def normalize(arr,min_=None, max_=None, axis=(0,2,3)):
@@ -237,39 +244,88 @@ def normalize_tmp(arr,min_=None, max_=None, axis=(0,2,3)):
         arr = (arr - min_) / (max_ - min_)
         return arr, min_, max_
 
-
-def plot_learn_curve(tr_losses, val_losses, save_dir='.'):
-    plt.clf()
-    plt.plot(tr_losses)
-    plt.plot(val_losses)
-    plt.savefig(save_dir + '/learn_curve.png')
-    plt.clf()
     
 
 import pdb
-    
-# little test/example
-if __name__ == "__main__":
-    """
-    for x,y in data_iterator(batch_size=1716, time_chunks_per_example=1):
+  
+
+  
+# # little test/example
+# if __name__ == "__main__":
+#     """
+#     for x,y in data_iterator(batch_size=1716, time_chunks_per_example=1):
         
-        # if any of the labels are not none
-        if y[y>-3].shape[0] > 0:
-            labelled_chunks = x[y>-3]
+#         # if any of the labels are not none
+#         if y[y>-3].shape[0] > 0:
+#             labelled_chunks = x[y>-3]
+#         print x.shape, y.shape
+#     """
+
+#     """
+#     i = 0
+#     for x,y in data_iterator(batch_size=1, time_chunks_per_example=8, img_size=-1, data_dir="/storeSSD/cbeckham/nersc/big_images/"):
+#         pdb.set_trace()
+#         i += 1
+#     print i
+#     assert i == 365*8
+#     """
+
+#     i=0
+#     for x1, x2 in consecutive_day_iterator_3d(start_day=1, end_day=10, data_dir="/storeSSD/cbeckham/nersc/big_images/", shuffle=False):
+#         print x1.shape, x2.shape
+#         i += 1
+#     print i
+if __name__ == "__main__":
+    for i, (x,y) in enumerate(data_iterator(batch_size=1,
+                      data_dir="/project/projectdirs/dasrepo/gordon_bell/deep_learning/data/climate/big_images/",
+                      time_chunks_per_example=8,
+                      shuffle=False,
+                      start_day=1,
+                      end_day=2,
+                      month1='10',
+                      day1='06',
+                      time_steps=8)):
         print x.shape, y.shape
-    """
+        plt.figure(i)
+        ax = plt.subplot(2,2,1)
+        ax.imshow(x[0,0,6])
+        for j in range(3):
+            ax = plt.subplot(2,2,j + 2)
+            ax.imshow(y[0,0,j], cmap='gray')
+        plt.show()
+        assert False
+    
 
-    """
-    i = 0
-    for x,y in data_iterator(batch_size=1, time_chunks_per_example=8, img_size=-1, data_dir="/storeSSD/cbeckham/nersc/big_images/"):
-        pdb.set_trace()
-        i += 1
-    print i
-    assert i == 365*8
-    """
+from pylab import rcParams
+rcParams['figure.figsize'] = 15, 25
+if __name__ == "__main__":
+    for (x,  y) in segmentation_iterator(1, 2, classes=2):
+        print x.shape, y.shape
+        plt.figure(1)
 
-    i=0
-    for x1, x2 in consecutive_day_iterator_3d(start_day=1, end_day=10, data_dir="/storeSSD/cbeckham/nersc/big_images/", shuffle=False):
-        print x1.shape, x2.shape
-        i += 1
-    print i
+        c = 0
+        for k in range(4):
+            ax = plt.subplot(4,8,c+1)
+            ax.imshow(x[0,k,6])
+            c+=1
+            for m in range(3):
+                
+                ax = plt.subplot(4,8,c+1)
+                ax.imshow(y[0,k,m], cmap='gray')
+                c+=1
+
+
+#         plt.figure(2)
+#         ax = plt.subplot(2,2,1)
+#         ax.imshow(x2[0,0,6])
+#         for j in range(3):
+#             ax = plt.subplot(2,2,j + 2)
+#             ax.imshow(y2[0,0,j], cmap='gray')
+#         plt.show()
+        assert False
+        
+
+
+
+
+
